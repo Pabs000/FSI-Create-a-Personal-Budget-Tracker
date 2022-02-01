951e99565189ec9eb9e5081e43da8fd0222b9ef2
Pabs000 <pgarcia2000.pg@gmail.comgit config --global user.name Pablo>
2022-01-31 19:10:41 -0800

    trying to see what I did in the webpage

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 100
+let monthlyExpenses = 400
+let annualExpenses = 4800
+
+
+
+window.prompt(weeklyExpenseQuestions[0])
+
+let answer = window.prompt(weeklyExpenseQuestions[0])
+console.log(answer)
+
+let stringAnswer = window.prompt(weeklyExpenseQuestions[0])
+let numberAnswer = parseFloat(stringAnswer)
+console.log(numberAnswer)
+
+
+for(i = 0;   i < weeklyExpenseQuestions.lenght;  i++           )
+
+{
+    let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+    weeklyExpenses = weeklyExpenses + answer
+}
+
+
 
 // Your Code Here
